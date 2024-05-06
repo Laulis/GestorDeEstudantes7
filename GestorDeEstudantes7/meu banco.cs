8988e15820c306a85d11dfb2866a89717250d1fd
Laulis <167126200+Laulis@users.noreply.github.com>
2024-05-06 15:25:50 -0300

    Introdução à conexão do Banco de Dados e Interface

---
+++ GestorDeEstudantes7/meu banco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstudantes7
+{
+    internal class meu_banco
+    {
+    }
+}
---
+++ GestorDeEstudantes7/meuBancoDeDados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GestorDeEstudantes7
+{
+    internal class meuBancoDeDados
+    {
+        //A conexão com o Banco de Dados.
+        MySqlConnection conexao = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=sga_estudantes_bd_t7");
+    }
+}
